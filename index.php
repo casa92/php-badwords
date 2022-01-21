f956cc8ae448313b138580f0411130538939a0ce
casa92 <casartelli92@gmail.com>
2022-01-21 15:20:54 +0100

    commenti

--- index.php
+++ index.php
@@ -9,10 +9,15 @@
 <body>
     
     <?php
+    //variabile contenente una frase
     $phrase = 'Lorem ipsum errore dolor sit amet errore consectetur, adipisicing elit. errore Accusantium accusamus vero, errore maxime, errore nemo recusandae voluptatem repudiandae minima sit illum voluptatibus cum id nulla distinctio? errore errore errore Facilis quis saepe ullam blanditiis iusto?';
+    //lunghezza di ciò che è contenuto nella variabile
     $phrase_length = strlen($phrase);
+    //parametro che da inserire con get
     $word_to_censore = $_GET['deletethisword'];
+    //funzione che sostituisce nella frase la parola scelta dall'utente con '***'
     $censored_phrase = str_replace($word_to_censore, '***', $phrase);
+    //lunghezza della nuova frase
     $censored_phrase_lenght = strlen($censored_phrase);
     
     ?>
@@ -20,7 +25,8 @@
     <h2>Frase scritta con PHP:</h2>
 
     <p>
-        <?php 
+        <?php
+            //stampo la frase 
             echo $phrase;
         ?>
     </p>
